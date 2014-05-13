49c7cb1172550c44bac3e19ce9bcc25869fd5628
David G Chaves <davidgchaves@gmail.com>
2014-05-13 18:11:04 +0200

    Pass test "it adds items to the list"

--- lib/list_server.ex
+++ lib/list_server.ex
@@ -11,6 +11,10 @@ defmodule ListServer do
     :gen_server.cast :my_list_server, :clear
   end
 
+  def add(item) do
+    :gen_server.cast :my_list_server, {:add_item, item}
+  end
+
   def items do
     :gen_server.call :my_list_server, :items
   end
@@ -23,6 +27,10 @@ defmodule ListServer do
 
   def handle_cast(:clear, _list) do
     {:noreply, []}
+  end
+
+  def handle_cast({:add_item, item}, list) do
+    {:noreply, list ++ [item]}
   end
 
   def handle_call(:items, _from, list) do
--- test/list_server_test.exs
+++ test/list_server_test.exs
@@ -9,4 +9,11 @@ defmodule ListServerTest do
     ListServer.start_link
     assert ListServer.items == []
   end
+
+  test "it adds items to the list" do
+    ListServer.start_link
+
+    ListServer.add "a book"
+    assert ListServer.items == ["a book"]
+  end
 end
