35bb6160da37b1e4514a38386e9d97531528197b
David G Chaves <davidgchaves@gmail.com>
2014-05-13 18:53:50 +0200

    Add 'crash_it!' to crash the server

--- lib/list_server.ex
+++ lib/list_server.ex
@@ -19,6 +19,10 @@ defmodule ListServer do
     :gen_server.cast :my_list_server, {:remove_item, item}
   end
 
+  def crash_it! do
+    :gen_server.cast :my_list_server, :provoke_crash
+  end
+
   def items do
     :gen_server.call :my_list_server, :items
   end
@@ -39,6 +43,10 @@ defmodule ListServer do
 
   def handle_cast({:remove_item, item}, list) do
     {:noreply, List.delete(list, item)}
+  end
+
+  def handle_cast(:provoke_crash, _list) do
+    1 = 2
   end
 
   def handle_call(:items, _from, list) do
